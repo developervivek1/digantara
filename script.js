3ef698ba7aa2804351a1ebd844faff788fe084e9
developervivek1 <vivekpathak.emmy1234@gmail.com>
2026-01-03 23:27:46 +0530

    portfolio section updated

--- script.js
+++ script.js
@@ -103,3 +103,137 @@ particlesJS("particles-js", {
     },
     "retina_detect": true
   });
+
+
+
+  // testimonials
+  jQuery( document ).ready( function ( $ ) {
+	const $feedbackSlider = $( '.feedback-slider' );
+
+	if ( $feedbackSlider.length ) {
+		$feedbackSlider.owlCarousel( {
+			items: 1,
+			nav: true,
+			dots: true,
+			autoplay: false,
+			loop: true,
+			mouseDrag: true,
+			touchDrag: true,
+			navText: [
+				'<i class="fa fa-arrow-left"></i>',
+				'<i class="fa fa-arrow-right"></i>'
+			],
+			responsive: {
+				// viewport >= 767px
+				767: {
+					nav: true,
+					dots: false
+				}
+			}
+		} );
+	
+		$feedbackSlider.on( 'translate.owl.carousel', function() {
+			$( '.feedback-slider-item h3' )
+				.removeClass( 'animated fadeIn' )
+				.css( 'opacity', '0' );
+	
+			$( '.feedback-slider-item img, .feedback-slider-thumb img, .customer-rating' )
+				.removeClass( 'animated zoomIn' )
+				.css( 'opacity', '0' );
+		} );
+	
+		$feedbackSlider.on( 'translated.owl.carousel', function() {
+			$( '.feedback-slider-item h3' )
+				.addClass( 'animated fadeIn' )
+				.css( 'opacity', '1' );
+	
+			$( '.feedback-slider-item img, .feedback-slider-thumb img, .customer-rating' )
+				.addClass( 'animated zoomIn' )
+				.css( 'opacity', '1' );
+		} );
+	
+		$feedbackSlider.on( 'changed.owl.carousel', function( property ) {
+			const current = property.item.index;
+	
+			const prevThumb = $( property.target )
+				.find( '.owl-item' )
+				.eq( current )
+				.prev()
+				.find( 'img' )
+				.attr( 'src' );
+	
+			const nextThumb = $( property.target )
+				.find( '.owl-item' )
+				.eq( current )
+				.next()
+				.find( 'img' )
+				.attr( 'src' );
+	
+			const prevRating = $( property.target )
+				.find( '.owl-item' )
+				.eq(current)
+				.prev()
+				.find( 'span' )
+				.attr( 'data-rating' );
+	
+			const nextRating = $( property.target )
+				.find( '.owl-item' )
+				.eq( current )
+				.next()
+				.find( 'span' )
+				.attr( 'data-rating' );
+	
+			$( '.thumb-prev' ).find( 'img' ).attr( 'src', prevThumb );
+			$( '.thumb-next' ).find( 'img' ).attr( 'src', nextThumb );
+	
+			$( '.thumb-prev' )
+				.find( 'span' )
+				.next()
+				.html( prevRating + '<i class="fa fa-star"></i>' );
+	
+			$( '.thumb-next' )
+				.find( 'span' )
+				.next()
+				.html( nextRating + '<i class="fa fa-star"></i>' );
+		} );
+	
+		$( '.thumb-next' ).on( 'click', function( e ) {
+			e.preventDefault();
+	
+			$feedbackSlider.trigger( 'next.owl.carousel', [ 300 ] );
+		} );
+	
+		$( '.thumb-prev' ).on( 'click', function( e ) {
+			e.preventDefault();
+	
+			$feedbackSlider.trigger( 'prev.owl.carousel', [ 300 ] );
+		} );
+	}
+} ); // end ready func
+
+
+
+// contact form
+
+		const handleSubmit = (event) => {
+			event.preventDefault();
+
+			const myForm = event.target;
+			const formData = new FormData(myForm);
+
+			fetch("/", {
+			method: "POST",
+			headers: { "Content-Type": "application/x-www-form-urlencoded" },
+			body: new URLSearchParams(formData).toString(),
+			})
+			.then(() => {
+				// SUCCESS: Do what you want here
+				alert("Thank you! Your message has been sent. We typically respond within 15 minutes.");
+				myForm.reset(); // Clear the form fields
+			})
+			.catch((error) => alert(error));
+		};
+
+		document.querySelector("form").addEventListener("submit", handleSubmit);
+
+//contact form end
